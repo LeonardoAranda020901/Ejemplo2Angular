3ca6440d3a98ea9d316b47763cffdfef0d9abf00
Leonardo <leonardoaranda6698@gmail.com>
2022-10-17 19:13:10 -0500

    Aranda

--- src/app/utl/main-page/main-page.component.ts
+++ src/app/utl/main-page/main-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-interface AlumnosUtl{
-  nombre:string;
-  edad:number;
-}
+import { AlumnosUtl } from '../interface/utl.interface';
+import { UtlService } from '../service/utl.service';
 
 @Component({
   selector: 'app-main-page',
@@ -11,44 +9,11 @@ interface AlumnosUtl{
 })
 export class MainPageComponent{
 
- /* <li>Lucia - 22</li>
-  <li>Leonardo - 21</li>
-  <li>Diana - 21</li>*/
-
-  alumnosUtl:AlumnosUtl[]=[
-    {
-      nombre:'Mario',
-      edad:15,
-  
-    },
-    {
-      nombre:'Lucia',
-      edad:15,
-  
-    },
-    {
-      nombre:'Leo Gei',
-      edad:15,
-  
-    },
-    {
-      nombre:'Littzy',
-      edad:15,
-  
-    },
-    ]
 regAlumno:AlumnosUtl={
   nombre:'',
   edad:0
 }
-agregar(){
-  //console.log(this.alumnosUtl)
-  this.alumnosUtl.push(this.regAlumno);
-  this.regAlumno={
-    nombre:'',
-    edad:0
-  }
-}
- 
 
+constructor(){}
+ 
 }
